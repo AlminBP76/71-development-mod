7b97e3c29cb82680b61be6ff1894fb3a3369d98e
AlminBP76 <71565182+AlminBP76@users.noreply.github.com>
2022-01-27 13:43:23 +0100

    Add files via upload

---
+++ resources/[skripte]/aCore/client/amenu-cl.lua
@@ -0,0 +1,252 @@
+RegisterNetEvent('notw:getajigrace')
+AddEventHandler('notw:getajigrace', function()
+    ESX.TriggerServerCallback('liberty:getajigrace', function(igraci)
+        local players = GetActivePlayers()
+        local menu = {}
+
+        TriggerEvent('nh-context:sendMenu', {
+            {
+                id = 0,
+                header = 'IGRACI >  '.. #players .. ' / 64',
+                txt = ''
+            },
+        })
+
+        if igraci ~= nil then
+            for i=1, #igraci, 1 do
+                table.insert(menu, {
+                    id = k,
+                    header = 'Ime Igraca > ' ..igraci[i].name,
+                    txt = 'Posao Igraca > ' ..igraci[i].posao..' : '..igraci[i].cin,
+                    params = {
+                        event = '',
+                        args = {}
+                    }
+                })
+            end
+            if #menu ~= 0 then
+                TriggerEvent('nh-context:sendMenu', menu)
+            else
+                TriggerEvent('nh-context:sendMenu', {
+                    {
+                        id = 1,
+                        header = 'Nema Igraca',
+                        txt = ''
+                    }
+                })
+            end
+        else
+            TriggerEvent('nh-context:sendMenu', {
+                {
+                    id = 1,
+                    header = 'Nema Igraca',
+                    txt = ''
+                }
+            })
+        end
+    end)
+end) 
+
+RegisterNetEvent('almin:healujIgraca')
+AddEventHandler('almin:healujIgraca', function(data)
+
+    local dialog = exports['zf_dialog']:DialogInput({
+        header = "Healuj Igraca", 
+        rows = {
+            {
+                id = 0, 
+                txt = "ID Igraca"
+            },
+        }
+    })
+    
+    if dialog ~= nil then
+        if dialog[1].input == nil then
+            ESX.ShowNotification('Ne mozete ostavljati prazna polja!')
+        else
+            TriggerEvent("esx_basicneeds:healPlayer", dialog[1].input)
+        end
+    end
+end)   
+
+AddEventHandler('dvAutaAdminMeni', function()
+	TriggerEvent('nh-context:sendMenu', {
+		{
+			id = 1,
+			header = "< Nazad",
+			txt = "",
+			params = {
+				event = "adminContextMeni",
+			}
+		},
+		{
+			id = 2,
+			header = "Obrisi trenutno vozilo",
+			txt = "",
+			params = {
+				event = "dvAdminMeni",
+			}
+		},
+		{
+			id = 3,
+			header = "Obrisi sva vozila",
+			txt = "",
+			params = {
+				event = "dvallAdminMeni",
+			}
+		},
+	})
+end)
+
+AddEventHandler('vozilaMeniAdmini', function()
+	TriggerEvent('nh-context:sendMenu', {
+		{
+			id = 1,
+			header = "< Nazad",
+			txt = "",
+			params = {
+				event = "adminContextMeni",
+			}
+		},
+		{
+			id = 2,
+			header = "Admin Motor",
+			txt = "Stvori motor za igraca (Sanchez)",
+			params = {
+				event = "motorAdminMeni",
+			}
+		},
+		{
+			id = 3,
+			header = "Admin Auto",
+			txt = "Stvori auto za igraca (Brioso)",
+			params = {
+				event = "voziloAdminMeni",
+			}
+		},
+	})
+end)
+
+RegisterCommand('+adminmeni', function()
+    ESX.TriggerServerCallback("aCore:pokupiRank", function(playerRank)
+      if playerRank == "owner" or playerRank == "superadmin" or playerRank == "admin" then
+		TriggerEvent("adminContextMeni")
+      else
+		exports['okokNotify']:Alert("NEMATE PRISTUP ADMIN MENIJA", "", 4500, 'info')
+      end
+    end)
+  end)
+
+  RegisterKeyMapping('+adminmeni', 'Otvori Admin Meni', 'keyboard', 'F3')
+
+  AddEventHandler('ideviAdminMeni', function()
+	ExecuteCommand('id')
+  end)
+
+  AddEventHandler('voziloAdminMeni', function()
+	TriggerEvent('esx:spawnVehicle', "brioso")
+  end)
+
+  AddEventHandler('motorAdminMeni', function()
+	TriggerEvent('esx:spawnVehicle', "sanchez2")
+  end)
+
+  AddEventHandler('dvAdminMeni', function()
+	ExecuteCommand('dv')
+  end)
+
+  AddEventHandler('dvallAdminMeni', function()
+	ExecuteCommand('dvall')
+  end)
+
+  --NH CONTEXT ADMIN MENI
+
+AddEventHandler('adminContextMeni', function()
+	TriggerEvent('nh-context:sendMenu', {
+		{
+			id = 1,
+			header = "Izaberi jednu od opcija",
+			txt = ""
+		},
+		{
+			id = 2,
+			header = "Noclip",
+			txt = "Udji / Izadji iz Noclipa",
+			params = {
+				event = "esx_adminduty:noclip",
+			}
+		},
+		{
+			id = 3,
+			header = "TPM",
+			txt = "Teleportiraj se na marker",
+			params = {
+				event = "esx_adminduty:tpm",
+			}
+		},
+		{
+		  id = 4,
+		  header = "Fix",
+		  txt = "Popravi Vozilo",
+		  params = {
+			  event = "almin:popravi",
+		  }
+	  },
+	  {
+		id = 5,
+		header = "Clean",
+		txt = "Operi Vozilo",
+		params = {
+			event = "almin:operi",
+		}
+	},
+	{
+		id = 6,
+		header = "Healuj Igraca",
+		txt = "Unesi ID igraca kojeg zelis da healujes",
+		params = {
+			event = "almin:healujIgraca",
+		}
+	},
+	{
+		id = 7,
+		header = "ID",
+		txt = "Ukljuci / Iskljuci ID-eve drugih igraca",
+		params = {
+			event = "ideviAdminMeni",
+		}
+	},
+	{
+		id = 8,
+		header = "Admin Vozila",
+		txt = "Pogledaj listu vozila dostupnih za spawnanje >",
+		params = {
+			event = "vozilaMeniAdmini",
+		}
+	},
+	{
+		id = 9,
+		header = "Brisanje Vozila",
+		txt = "Pogledaj opcije za brisanje vozila >",
+		params = {
+			event = "dvAutaAdminMeni",
+		}
+	},
+	{
+		id = 10,
+		header = "Spectate",
+		txt = "Posmatraj igrace",
+		params = {
+			event = "esx_spectate:spectate",
+		}
+	},
+	{
+		id = 11,
+		header = "Lista igrica",
+		txt = "Pogledaj igrace koji su trenutno na serveru >",
+		params = {
+			event = "notw:getajigrace",
+		}
+	}
+	})
+end)
