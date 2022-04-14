2ed8e126687a21586a9423297d7606a653cd3349
AlminBP76 <71565182+AlminBP76@users.noreply.github.com>
2022-04-14 12:44:10 +0200

    Izbačen pe lua lib,ubačen ox_lib
    Izbačen pe lua lib,ubačen ox_lib

---
+++ resources/[skripte]/ox_lib/resource/interface/client/context.lua
@@ -0,0 +1,45 @@
+local contextMenus = {}
+
+function lib.showContext(id)
+    local data = contextMenus[id]
+    SetNuiFocus(true, true)
+    SendNUIMessage({
+        action = 'showContext',
+        data = {
+            title = data.title,
+            menu = data.menu,
+            options = data.options
+        }
+    })
+end
+
+function lib.registerContext(context)
+    for k, v in pairs(context) do
+        if type(k) == 'number' then
+            contextMenus[v.id] = v
+        else
+            contextMenus[context.id] = context
+            break
+        end
+    end
+end
+
+RegisterNUICallback('openContext', function(id, cb)
+    cb(1)
+    lib.showContext(id)
+end)
+
+RegisterNUICallback('clickContext', function(data, cb)
+    cb(1)
+    if data.event then TriggerEvent(data.event, data.args) end
+    if data.serverEvent then TriggerServerEvent(data.serverEvent, data.args) end
+    SetNuiFocus(false, false)
+    SendNUIMessage({
+        action = 'hideContext'
+    })
+end)
+
+RegisterNUICallback('closeContext', function(_, cb)
+    cb(1)
+    SetNuiFocus(false, false)
+end)
